80341204c53d2d941d0a2ae1aaa17520704c12f6
stuckDaemon <gabriele.bonadiman@gmail.com>
2025-07-23 22:22:06 +0200

    chore(deployment): wip aws deployment

--- backend/src/config/database/database.js
+++ backend/src/config/database/database.js
@@ -1,26 +1,27 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires,@typescript-eslint/no-require-imports
 require('dotenv').config();
 
-const config = {
-  local: {
-    logging: false,
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: 'postgres',
-    define: {
-      timestamps: true,
-    },
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false,
-      },
-    },
-    seederStorage: 'sequelize',
+const common = {
+  logging: false,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  dialect: 'postgres',
+  define: {
+    timestamps: true,
   },
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+  seederStorage: 'sequelize',
 };
 
-module.exports = config;
+module.exports = {
+  development: common,
+  production: common,
+  test: common,
+};
